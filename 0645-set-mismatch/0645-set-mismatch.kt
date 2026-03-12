85f3e387a3cde82a4614a5f3adcd030a31457596
Opu Hossain <opu.hossain.data@gmail.com>
2026-03-12 14:56:48 +0600

    Time: 1 ms (100%), Space: 48.7 MB (86.26%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.kt
+++ 0645-set-mismatch/0645-set-mismatch.kt
@@ -1,19 +1,28 @@
 class Solution {
     fun findErrorNums(nums: IntArray): IntArray {
-       val freq = mutableMapOf<Int, Int>()
+       /* We will use the summation formula of the linear series, which is n*(n+1)/2 and n*(n+1)*(2n+1)/6.
+            and then follow along the algorithm...
+         */
+        var acSum = 0L
+        var acSumSqr = 0L
+        val arraySize = nums.size.toLong()
 
-        for (i in nums) {
-            freq[i] = freq.getOrDefault(i, 0) + 1
-        }
-        var duplicate = -1
-        var missing = -1
 
-        for (n in 1..nums.size) {
-            when(freq[n]) {
-                2 -> duplicate = n
-                null -> missing = n
-            }
+        for(i in nums) {
+            acSum += i.toLong()
+            acSumSqr += i.toLong() * i.toLong()
         }
-        return intArrayOf(duplicate, missing)
+        val expSum = arraySize * (arraySize + 1) / 2
+        val expSumSqr = arraySize * (arraySize + 1) * (2 * arraySize + 1) / 6
+
+        val difference1 = acSum - expSum
+        val difference2 = acSumSqr - expSumSqr
+
+        val difference = difference2 / difference1
+
+        val duplicateNumber = (difference1 + difference) / 2
+        val missingNumber = difference - duplicateNumber
+
+        return intArrayOf(duplicateNumber.toInt(),missingNumber.toInt())
     }
 }
