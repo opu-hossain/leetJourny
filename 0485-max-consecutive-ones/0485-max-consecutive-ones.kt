c2bacf0af79a051d87d8b6eeb6152d2db3f780db
Opu Hossain <opu.hossain.data@gmail.com>
2026-03-12 12:50:19 +0600

    Time: 2 ms (100%), Space: 53.2 MB (88.81%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.kt
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun findMaxConsecutiveOnes(nums: IntArray): Int {
-        var count = 0
-        var resultCount = 0
+        var count = 0 // Current streak
+        var resultCount = 0 // Stored streak
         for (i in nums) {
             when(i) {
                 1 -> count++
-                0 -> {if (resultCount < count) resultCount = count
+                0 -> {if (resultCount < count) resultCount = count // Checking if the current streak is greater than stored streak.
                 count = 0}
             }
         }
-        return maxOf(resultCount, count)
+        return maxOf(resultCount, count) // in case of last the last digit being '1' we trurn the max from the both variable
     }
 }
